25d178184e98905d16c8be558916a5ba181762e1
beyanon <tokabe333@gmail.com>
2020-12-30 00:14:19 +0900

    update : ngongo

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,8 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { FormsModule } from "@angular/forms";
+import { MatDialogModule } from "@angular/material/dialog";
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +12,8 @@ import { DashboardComponent } from './dashboard/dashboard.component';
 import { UserListComponent } from './user/user-list/user-list.component';
 import { UserEditComponent } from './user/user-edit/user-edit.component';
 
-import { UserService } from "./user/user.service"
+import { UserService } from "./user/user.service";
+import { ConfirmDialogComponent } from './tools/confirm-dialog/confirm-dialog.component'
 
 @NgModule({
 	declarations: [
@@ -20,14 +23,20 @@ import { UserService } from "./user/user.service"
 		DashboardComponent,
 		UserListComponent,
 		UserEditComponent,
+		ConfirmDialogComponent,
 	],
 	imports: [
 		BrowserModule,
 		AppRoutingModule,
 		FormsModule,
+		MatDialogModule,
+		BrowserAnimationsModule,
 	],
 	providers: [
 		UserService,
+	],
+	entryComponents: [
+		ConfirmDialogComponent,
 	],
 	bootstrap: [AppComponent]
 })
